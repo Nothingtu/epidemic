8e2ec43aa2925871a3aa77c50605258f929fa22a
Nothingtu <18268174991@139.com>
2020-11-14 23:28:42 +0800

    项目第二次提交

---
+++ src/main/java/com/duing/mapper/DataMapper.java
@@ -0,0 +1,24 @@
+package com.duing.mapper;
+
+
+import com.duing.entity.DataBean;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface DataMapper {
+
+   @Select("SELECT ID ,NAME,CONFIRM,NOWCONFIRM,DEAD,HEAL FROM EPIDEMIC")
+    List<DataBean> selectList();
+
+   @Update("INSERT INTO EPIDEMIC VALUES(null,#{name},#{confirm},#{nowConfirm},#{dead},#{heal}) ")
+    int insert(DataBean dataBean);
+
+   @Delete("DELETE FROM EPIDEMIC")
+    int delete();
+
+}
